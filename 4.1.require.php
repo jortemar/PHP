a2592f72303fdbb939d3656ce3a3d90caebc42a0
jortemar <jortemar@gmail.com>
2022-10-05 19:20:45 +0200

    borrado archivos

--- 4.1.require.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // require llama un archivo externo y lo incrusta
-    
-    $a = "variable del principal";
-    require "4.2.ejerequerido.php";
-    $b = "otra variable del principal";
-    echo "En el script principal";
